c3fcc7b5f4ec9f428bd840aa91833ba775c85692
hrmn1210 <15220648@bsi.ac.id>
2024-03-25 17:50:03 +0700

    tugas1

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -4,9 +4,6 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
-        echo "Nama saya Imam Nawawi
- Saya tingga di daerah Ciputat
- olah raga yang saya sukai adalah
- Bulutangkis";
+        echo "Nama saya herman firmansyah saya tinggak di purnajaya 1 jalur 2 no.28, saya adalah seorang mahasiswa ubsi semester 4, dan ini adala tugas pertama makul WP2";
     }
 }
