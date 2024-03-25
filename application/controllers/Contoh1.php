69b05cdc3790e028e212a024b2fff59e0b7a533f
hrmn1210 <15220648@bsi.ac.id>
2024-03-25 18:01:16 +0700

    tugas1

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -4,6 +4,9 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
-        echo "Nama saya herman firmansyah saya tinggak di purnajaya 1 jalur 2 no.28, saya adalah seorang mahasiswa ubsi semester 4, dan ini adala tugas pertama makul WP2";
+        echo "Nama saya herman firmansyah <br>
+        saya tinggal di purnajaya 1 jalur 2 no.28, <br>
+        saya adalah seorang mahasiswa ubsi semester 4,<br>
+         dan ini adala tugas pertama makul WP2";
     }
 }
