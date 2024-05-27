84176fa94e83aa2a4b8e30e4aeee9c66747dd402
hrmn1210 <15220648@bsi.ac.id>
2024-05-27 16:50:39 +0700

    pertemuan 10 dan tugas 8

--- application/views/autentifikasi/login.php
+++ application/views/autentifikasi/login.php
@@ -30,6 +30,9 @@
                                 </form>
                                 <hr>
                                 <div class="text-center">
+                                    <a class="small" href="<?= base_url('autentifikasi/lupaPassword'); ?>">Lupa Password?</a>
+                                </div>
+                                <div class="text-center">
                                     <a class="small" href="<?= base_url('autentifikasi/registrasi'); ?>">Daftar Member!</a>
                                 </div>
                             </div>
